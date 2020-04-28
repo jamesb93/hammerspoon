a95347d548df6c96cdbcac0469c5b390a6c6a680
James Bradbury <jamesbradbury93@gmail.com>
2020-04-28 18:30:41 +0100

    change directory of pomolog

--- pomodoro.lua
+++ pomodoro.lua
@@ -6,7 +6,7 @@ pomo.params = {
     work_dur = 60 * 20, -- 60 seconds * 20 minutes
     srest_dur = 60 * 5, -- 60 seconds * 5 minutes
     lrest_dur = 60 * 15, -- 60 seconds * 15 minute
-    logpath = "/Users/james/.pomolog",
+    logpath = "/Users/james/.pomo/.pomolog",
     timecoef = 1,
     chronoid = "pomodatetimeid"
 }
