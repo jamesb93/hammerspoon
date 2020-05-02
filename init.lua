4a4a98cb0a9e6df36e21dfe60a3df18d30c2b536
James Bradbury <jamesbradbury93@gmail.com>
2020-05-02 16:11:42 +0100

    add skhd in init

--- init.lua
+++ init.lua
@@ -1,25 +1,23 @@
-require("battery")
+-- require("battery")
 require("caffeine")
 require("keep_quiet")
 require("pomodoro")
+require("skhd")
 -- require("backup")
 
 -- Spectacle Replacement
-custom_bindings = {
-	leftHalf = {
-		{{"cmd"}, "left"}
-	},
-	rightHalf = {
-		{{"cmd"}, "right"}
-	},
-	fullScreen = {
-		{{"cmd"}, "return"}
-	},
-	center = {
-		{{"cmd", "alt"}, "c"}
-	},
-	undo = false,
-	redo = false
-}
-hs.loadSpoon("Lunette")
-spoon.Lunette:bindHotkeys(custom_bindings)
+-- custom_bindings = {
+-- 	leftHalf = {
+-- 		{{"cmd"}, "left"}
+-- 	},
+-- 	rightHalf = {
+-- 		{{"cmd"}, "right"}
+-- 	},
+-- 	fullScreen = {
+-- 		{{"cmd"}, "return"}
+-- 	},
+-- 	undo = false,
+-- 	redo = false
+-- }
+-- hs.loadSpoon("Lunette")
+-- spoon.Lunette:bindHotkeys(custom_bindings)
