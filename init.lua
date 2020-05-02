2eb9452f20b55778b566607f3d526981c702bc0b
James Bradbury <jamesbradbury93@gmail.com>
2020-05-03 00:01:19 +0100

    use skhd and yabai no spectacle

--- init.lua
+++ init.lua
@@ -3,21 +3,3 @@ require("caffeine")
 require("keep_quiet")
 require("pomodoro")
 require("skhd")
--- require("backup")
-
--- Spectacle Replacement
--- custom_bindings = {
--- 	leftHalf = {
--- 		{{"cmd"}, "left"}
--- 	},
--- 	rightHalf = {
--- 		{{"cmd"}, "right"}
--- 	},
--- 	fullScreen = {
--- 		{{"cmd"}, "return"}
--- 	},
--- 	undo = false,
--- 	redo = false
--- }
--- hs.loadSpoon("Lunette")
--- spoon.Lunette:bindHotkeys(custom_bindings)
