9bb9030aa1594c74e5d16e611f4ee82f99c95160
James Bradbury <github@jamesbradbury.net>
2022-07-17 21:15:27 +0100

    update audiofuzzer

--- audiofuzz.lua
+++ audiofuzz.lua
@@ -1,6 +1,5 @@
 hyper = {"cmd", "shift"}
 
-
 function bindCmd(char, cmd)
   hs.hotkey.bind(hyper, char, function()
     hs.execute(cmd, true)
@@ -22,23 +21,18 @@ function showAudioFuzzy()
   local active_input = hs.audiodevice.defaultInputDevice()
   local active_output = hs.audiodevice.defaultOutputDevice()
   local active, subtext
+   
   for i=1, #devices do
     if devices[i]:isOutputDevice() then
       active = devices[i]:uid() == active_output:uid()
-      subtext = "output"
-    else
-      active = devices[i]:uid() == active_input:uid()
-      subtext = "input"
+      if active then subtext = "Active" else subtext = "" end
+      choices[#choices+1] = {
+        text = devices[i]:name(),
+        uid = devices[i]:uid(),
+        subText = subtext,
+        valid = not active,
+      }
     end
-    if active then
-      subtext = subtext .. " (active)"
-    end
-    choices[i] = {
-      text = devices[i]:name(),
-      uid = devices[i]:uid(),
-      subText = subtext,
-      valid = not active,
-    }
   end
   fuzzy(choices, selectAudio)
 end
@@ -48,11 +42,8 @@ function selectAudio(audio)
     return
   end
   local device = hs.audiodevice.findDeviceByUID(audio.uid)
-  hs.alert.show("Setting "..audio.subText.." device: "..device:name())
   if device:isOutputDevice() then
     device:setDefaultOutputDevice()
-  else
-    device:setDefaultInputDevice()
   end
 end
 
